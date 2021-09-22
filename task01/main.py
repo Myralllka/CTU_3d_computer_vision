519f7a56061464cdede33acada91b7aa48a9cbc4
Mykola Morgunenko <Olli25010@gmail.com>
2021-09-22 10:11:33 +0200

    completed 01 without homography

--- task01/main.py
+++ task01/main.py
@@ -22,6 +22,46 @@ def plt_rectangle(p1, p2, p3, p4, marker='b-'):
     plt_line(p4, p1, marker)
 
 
+def distance(a, b):
+    # a[x, y], b[x, y]
+    return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+
+def is_between(a, c, b):
+    return distance(a, c) + distance(c, b) == distance(a, b)
+
+
+def is_point_on_rect(rect, point):
+    # rect: lu ru rd ld
+    return (is_between(rect[0], point, rect[1]) or
+            is_between(rect[1], point, rect[2]) or
+            is_between(rect[2], point, rect[3]) or
+            is_between(rect[3], point, rect[0]))
+
+
+def is_point_in_rect(rect, point):
+    x1, y1 = rect[0]
+    x2, y2 = rect[2]
+    x, y = point
+    if x1 < x < x2:
+        if y1 < y < y2:
+            return True
+    return False
+
+
+def plt_line_in_rectangle(rect, a_local, b_local, marker='b-'):
+    cs, cs_new = [], []
+    cs.append(get_intersect_lines(rect[0], rect[1], a_local, b_local))
+    cs.append(get_intersect_lines(rect[1], rect[2], a_local, b_local))
+    cs.append(get_intersect_lines(rect[2], rect[3], a_local, b_local))
+    cs.append(get_intersect_lines(rect[3], rect[0], a_local, b_local))
+    for each in cs:
+        if is_point_on_rect(rect, each):
+            cs_new.append(each)
+
+    plt.plot([cs_new[0][0], cs_new[1][0]], [cs_new[0][1], cs_new[1][1]])
+
+
 if __name__ == "__main__":
     fig = plt.figure()
     bound_points = np.array([[1, 1, 800, 800], [1, 600, 600, 1]])
@@ -44,11 +84,11 @@ if __name__ == "__main__":
         pts.append((a, b))
 
     intersection = get_intersect_lines(pts[0][0], pts[0][1], pts[1][0], pts[1][1])
+    if is_point_in_rect(bound_points.T, intersection):
+        plt.plot(intersection[0], intersection[1], "ro", mfc='none')
 
-    plt.plot(intersection[0], intersection[1], "ro", mfc='none')
-
-    plt_line(pts[0][0], pts[0][1], "{}-".format(colors[0]))
-    plt_line(pts[1][0], pts[1][1], "{}-".format(colors[1]))
+    plt_line_in_rectangle(bound_points.T, pts[0][0], pts[0][1])
+    plt_line_in_rectangle(bound_points.T, pts[1][0], pts[1][1])
     tellme('intersection')
     # numpy.cross()
     plt.show()
