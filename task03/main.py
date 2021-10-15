2f2857dbfa7f42152a0e38dd5f1ff6f8759ee32a
Mykola Morgunenko <Olli25010@gmail.com>
2021-10-15 16:13:25 +0200

    started task3

--- task03/main.py
+++ task03/main.py
@@ -1,7 +1,29 @@
 import numpy as np
 
 from toolbox import *
-from mpl_toolkits import mplot3d
+
+
+def fit_line(Xs, Ys):
+    X_ = sum(Xs)
+    Y_ = sum(Ys)
+    x_Xs = Xs - X_
+    y_Ys = Ys - Y_
+    xXyY = x_Xs * y_Ys
+    x_X2 = x_Xs * x_Xs
+    k = sum(xXyY) / sum(x_X2)
+    b = Y_ - (k * X_)
+    return k, b
+
 
 if __name__ == "__main__":
-    pass
+    points = np.loadtxt('task03/data/linefit_3.txt').T
+    # points = np.array([[3, 4, 2, 6], [0, 1, 2, 3]])
+    plt.plot(points[0], points[1], '.')
+
+    k, b = fit_line(points[0], points[1])
+    np_k, np_b = np.polyfit(points[0], points[1], deg=1)
+
+    plt.plot(points[0],  k * points[0] + b, 'r.')
+    plt.plot(points[0],  np_k * points[0] + np_b, 'r.')
+
+    plt.show()
--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,5 +1,4 @@
 from toolbox.tools import *
-import numpy as np  # for matrix computation and linear algebra
 import matplotlib.pyplot as plt  # for drawing and image I/O
 import matplotlib.patches as patches
 import scipy.linalg as slinalg
