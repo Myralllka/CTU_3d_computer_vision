80554e11da0372eb597855b7b052f689c447af6c
Mykola Morgunenko <Olli25010@gmail.com>
2021-12-15 11:07:55 +0100

    make up to date

--- p5_impl/python/p5/__init__.py
+++ p5_impl/python/p5/__init__.py
@@ -1,8 +1,7 @@
 from p5.ext import *
 import numpy as np
 
-
-def p5gb(u1, u2):
+def p5gb( u1, u2 ):
     """
     Five-point calibrated relative pose problem (Grobner basis).
     Es = p5gb( u1, u2 ) computes the esential matrices E according to 
@@ -15,43 +14,43 @@ def p5gb(u1, u2):
       Es .. list of possible essential matrices
     """
 
-    q = np.vstack((u1[0] * u2[0], u1[1] * u2[0], u1[2] * u2[0],
-                   u1[0] * u2[1], u1[1] * u2[1], u1[2] * u2[1],
-                   u1[0] * u2[2], u1[1] * u2[2], u1[2] * u2[2])).T
+    q = np.vstack( ( u1[0] * u2[0], u1[1] * u2[0], u1[2] * u2[0],
+                     u1[0] * u2[1], u1[1] * u2[1], u1[2] * u2[1],
+                     u1[0] * u2[2], u1[1] * u2[2], u1[2] * u2[2] ) ).T
 
-    U, S, Vt = np.linalg.svd(q)
+    U, S, Vt = np.linalg.svd( q )
 
-    XYZW = Vt[5:, :]
+    XYZW = Vt[5:,:]
 
-    A = p5_matrixA(XYZW)  # in/out is transposed (numpy data are row-wise)
+    A = p5_matrixA( XYZW ) # in/out is transposed (numpy data are row-wise)
 
     A = A[[5, 9, 7, 11, 14, 17, 12, 15, 18, 19]] @ \
-        np.linalg.inv(A[[0, 2, 3, 1, 4, 8, 6, 10, 13, 16]])
+            np.linalg.inv( A[[0, 2, 3, 1, 4, 8, 6, 10, 13, 16]] )
 
     A = A.T
 
-    M = np.zeros((10, 10))
+    M = np.zeros( (10,10) )
     M[:6] = -A[[0, 1, 2, 4, 5, 7]]
 
-    M[6, 0] = 1
-    M[7, 1] = 1
-    M[8, 3] = 1
-    M[9, 6] = 1
+    M[6,0] = 1
+    M[7,1] = 1
+    M[8,3] = 1
+    M[9,6] = 1
 
-    D, V = np.linalg.eig(M)
+    D, V = np.linalg.eig( M )
 
     ok = np.imag(D) == 0.0
-    V = np.real(V)
+    V = np.real( V )
 
-    SOLS = V[6:9, ok] / V[9:10, ok]
+    SOLS = V[6:9,ok] / V[9:10,ok]
 
     SOLS = SOLS.T
 
     Evec = SOLS @ XYZW[:3] + XYZW[3]
 
-    Es = [None] * Evec.shape[0]
-    for i in range(0, Evec.shape[0]):
-        Es[i] = Evec[i].reshape((3, 3))
-        Es[i] = Es[i] / np.sqrt(np.sum(Es[i] ** 2))
+    Es = [ None ] * Evec.shape[0]
+    for i in range( 0, Evec.shape[0] ):
+        Es[i] = Evec[i].reshape((3,3))
+        Es[i] = Es[i] / np.sqrt( np.sum( Es[i]**2 ) )
 
     return Es
