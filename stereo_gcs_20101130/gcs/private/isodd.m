9af41ac8412b3f3fcb653c6998d939b159a167fc
Mykola Morgunenko <Olli25010@gmail.com>
2021-12-15 11:08:16 +0100

    make up to date

--- stereo_gcs_20101130/gcs/private/isodd.m
+++
@@ -1,7 +0,0 @@
-function odd = isodd(x)
-% ISODD(x) returns matrix where ones correspond to odd elements in x
-%	   zero is considered even
-
-% October 29, 1994: Radim Sara, CVL Prague
-
-odd = (x == fix(x)) & (x/2 ~= fix(x/2));
